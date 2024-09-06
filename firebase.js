8ce2434ece45e82508e8ace22164fbcdf61447b0
Savio09 <declanfortune5@gmail.com>
2024-09-06 13:18:05 -0700

    removed the exposed keys

--- firebase.js
+++ firebase.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBQVFnEdJ9xVzXtU6fb368TQbwEAdntAt4",
-  authDomain: "next-pantry-tracker.firebaseapp.com",
-  projectId: "next-pantry-tracker",
-  storageBucket: "next-pantry-tracker.appspot.com",
-  messagingSenderId: "498894959676",
-  appId: "1:498894959676:web:34b886a2aa60722edc3f39",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
